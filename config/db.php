f5e1ea0f1c1cc308636fade69d5344a35a34f1ab
vecherskyy <vecherskyy@mail.ru>
2017-02-15 10:18:57 +0200

    Delete db.php
    delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=demo',
-    'username' => 'root',
-    'password' => '87126534',
-    'charset' => 'utf8',
-];
